e10787176f30ecf3e2512ddec69ec8c0a9391811
hjcwinner <hjcwinner@naver.com>
2020-11-08 23:41:16 +0900

    Navbar제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Landing from './components/layout/Landing'
+import Navbar from './components/layout/Navbar'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Main from './components/screen/Main'
+import Questionnaire from './components/screen/Questionnaire'
+
 
 function App() {
   return (
-    <div className="App">
-      start
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route path="/" exact component={Landing} />
+        <section>
+          <Switch>
+            <Route path="/Main" exact component={Main} />
+            <Route path="/Questionnaire" exact component={Questionnaire} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />          
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+    
   );
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Navbar = (props) => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar bg-dark">
+            <h1>
+                <Link to="/">
+                    <i className='fas fa-code' /> LXPER
+                </Link>             
+            </h1>
+            <ul>
+                <li>
+                    <Link to="/main">문제목록</Link>
+                </li>
+                <li>
+                    <Link to="/questionnaire">문제출제</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>              
+            </ul>
+        </nav>
+        
     );
 };
 
