6b644296bc88183e014c12e9d811f661dd76d510
hjcwinner <hjcwinner@naver.com>
2020-11-08 23:51:08 +0900

    Landing페이지 만들기

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Landing = (props) => {
     return (
-        <div>
-            Landing
-        </div>
+        <section className='landing'>
+            <div className='dark-overlay'>
+                <div className='landing-inner'>
+                    <h1 className='x-large'>LXPER 문제은행</h1>
+                    <p className='lead'>
+                        LXPER 문제은행 하드코딩
+                    </p>
+                    <div>
+                        <Link to="/register" className='btn btn-primary'>
+                            Sing up
+                        </Link>
+                        <Link to='login' className='btn btn-light'>
+                            Login
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
