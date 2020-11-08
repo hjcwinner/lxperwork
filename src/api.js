b74fdea2f9ef81d95375ab28ff09d34d73b44691
hjcwinner <hjcwinner@naver.com>
2020-11-09 00:07:53 +0900

    api제작

---
+++ src/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const makeRequest = (path, params) =>
+    axios.get(`https://interview.lxper.ai/${path}`, {
+        params: {
+            ...params
+        }
+    })
+
+const getAnything = async (path, params = {}) => {
+    try {
+        const {
+            data
+        } = await makeRequest(path, params)
+        return [data, null]
+    }
+    catch(e) {
+        return [null, e]
+    }
+}
+
+export const lxperApi = {
+    getscreen : () => getAnything("/api/questions"),
+    postscreen : () => getAnything("/api/questions"),
+    postid : () => getAnything(`/api/questions/${id}`),
+    postdel : () => getAnything(`/api/questions/${id}`)
+}
