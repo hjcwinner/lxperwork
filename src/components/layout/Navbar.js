6401f1959af416f1a78276827af9ce8f6ac552c7
hjcwinner <hjcwinner@naver.com>
2020-11-14 14:39:30 +0900

    final

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = (props) => {
+const Navbar = () => {
 
     // const token = localStorage.getItem('token')
 
